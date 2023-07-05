dff19e814e273a934e57cb851b3777e17216d42f
priyanshu Ranjan <priyanshuranjan.dbg@gmail.com>
2023-07-05 17:21:40 +0530

    Added Emoji

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,8 +5,8 @@ function Home(props) {
     <>
     <div style={props.myStyle}>
     <h1>{`Welcome To Shopping Mart`}</h1>
-    <p><b>Have A look At Best Products</b> </p>
-    <h3>Thanks For Visiting</h3>
+    <p><b>Have A look At Best Products👕👚</b> </p>
+    <h3>😊Thanks For Visiting 😊</h3>
     </div>
     </>
   )
